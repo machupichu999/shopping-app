8916e1b2feccb9d000b6beb741d98aba2ea80853
machupichu999 <138388056+machupichu999@users.noreply.github.com>
2023-12-14 02:06:55 +0100

    Create models

--- server/src/main/java/org/shopping/server/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.shopping.server.repositories;
-
-import org.shopping.server.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
